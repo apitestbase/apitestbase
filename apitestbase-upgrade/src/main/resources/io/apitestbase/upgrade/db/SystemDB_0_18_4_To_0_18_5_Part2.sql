53e1c906a2ef50760670b2b2272d1fadeab5c0fa
Zheng Wang <zheng-wang@users.noreply.github.com>
2021-08-22 11:21:11 +0800

    refine code

--- apitestbase-upgrade/src/main/resources/io/apitestbase/upgrade/db/SystemDB_0_18_4_To_0_18_5_Part2.sql
+++ apitestbase-upgrade/src/main/resources/io/apitestbase/upgrade/db/SystemDB_0_18_4_To_0_18_5_Part2.sql
@@ -7,4 +7,7 @@ where type = 'MQ' and action in ('Enqueue', 'Publish') and request_type = 'Text'
 update teststep set other_properties = replace(other_properties, substring(other_properties from locate(',"rfh2Header":', other_properties)), '') || '}'
 where type = 'MQ' and action in ('Enqueue', 'Publish');
 
-update teststep set step_data_backup = null where type = 'MQ';
+update teststep set request = null, step_data_backup = null where type = 'MQ';
+
+ALTER TABLE TESTSTEP DROP COLUMN "REQUEST_TYPE";
+ALTER TABLE TESTSTEP DROP COLUMN "REQUEST_FILENAME";
