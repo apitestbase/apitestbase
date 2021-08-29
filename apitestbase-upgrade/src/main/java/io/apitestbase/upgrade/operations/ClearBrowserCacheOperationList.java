5897c5c5c87b22df975b17a431467c73ced8037b
Zheng Wang <zheng-wang@users.noreply.github.com>
2021-08-29 11:15:42 +0800

    update upgrade

--- apitestbase-upgrade/src/main/java/io/apitestbase/upgrade/operations/ClearBrowserCacheOperationList.java
+++ apitestbase-upgrade/src/main/java/io/apitestbase/upgrade/operations/ClearBrowserCacheOperationList.java
@@ -5,5 +5,6 @@ import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 public class ClearBrowserCacheOperationList extends OperationList {
     public ClearBrowserCacheOperationList() {
         getOperationList().add(new Operation(new DefaultArtifactVersion("0.18.3"), new DefaultArtifactVersion("0.18.4")));
+        getOperationList().add(new Operation(new DefaultArtifactVersion("0.18.4"), new DefaultArtifactVersion("0.18.5")));
     }
 }
