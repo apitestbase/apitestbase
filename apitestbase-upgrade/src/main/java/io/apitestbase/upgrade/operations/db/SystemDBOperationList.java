118fdb1a04f8b05ca2c1bd68e798eeca327b0b13
Zheng Wang <zheng-wang@users.noreply.github.com>
2021-10-13 20:12:09 +0800

    support cross-testcase UDPs

--- apitestbase-upgrade/src/main/java/io/apitestbase/upgrade/operations/db/SystemDBOperationList.java
+++ apitestbase-upgrade/src/main/java/io/apitestbase/upgrade/operations/db/SystemDBOperationList.java
@@ -22,5 +22,8 @@ public class SystemDBOperationList extends OperationList {
         getOperationList().add(new SimpleSystemDBOperation(
                 new DefaultArtifactVersion("0.18.7"), new DefaultArtifactVersion("0.19.0"),
                 "io/apitestbase/upgrade/db/SystemDB_0_18_7_To_0_19_0.sql"));
+        getOperationList().add(new SimpleSystemDBOperation(
+                new DefaultArtifactVersion("0.18.7"), new DefaultArtifactVersion("0.19.0"),
+                "io/apitestbase/upgrade/db/SystemDB_0_18_7_To_0_19_0_Part2.sql"));
     }
 }
