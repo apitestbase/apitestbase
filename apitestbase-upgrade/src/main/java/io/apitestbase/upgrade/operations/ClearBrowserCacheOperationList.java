ae27e33dca85a58e4beac4090afa55695a2c130b
Zheng Wang <zheng-wang@users.noreply.github.com>
2022-01-26 17:18:51 +0800

    refine code

--- apitestbase-upgrade/src/main/java/io/apitestbase/upgrade/operations/ClearBrowserCacheOperationList.java
+++ apitestbase-upgrade/src/main/java/io/apitestbase/upgrade/operations/ClearBrowserCacheOperationList.java
@@ -3,10 +3,15 @@ package io.apitestbase.upgrade.operations;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 
 public class ClearBrowserCacheOperationList extends OperationList {
+    private void addOperation(String oldVersion, String newVersion) {
+        getOperationList().add(new Operation(
+                new DefaultArtifactVersion(oldVersion), new DefaultArtifactVersion(newVersion)));
+    }
     public ClearBrowserCacheOperationList() {
-        getOperationList().add(new Operation(new DefaultArtifactVersion("0.18.3"), new DefaultArtifactVersion("0.18.4")));
-        getOperationList().add(new Operation(new DefaultArtifactVersion("0.18.4"), new DefaultArtifactVersion("0.18.5")));
-        getOperationList().add(new Operation(new DefaultArtifactVersion("0.18.5"), new DefaultArtifactVersion("0.18.6")));
-        getOperationList().add(new Operation(new DefaultArtifactVersion("0.18.7"), new DefaultArtifactVersion("0.19.0")));
+        addOperation("0.18.3", "0.18.4");
+        addOperation("0.18.4", "0.18.5");
+        addOperation("0.18.5", "0.18.6");
+        addOperation("0.18.7", "0.19.0");
+        addOperation("0.19.0", "0.20.0");
     }
 }
--- apitestbase-upgrade/src/main/java/io/apitestbase/upgrade/operations/db/SystemDBOperationList.java
+++ apitestbase-upgrade/src/main/java/io/apitestbase/upgrade/operations/db/SystemDBOperationList.java
@@ -4,27 +4,20 @@ import io.apitestbase.upgrade.operations.OperationList;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 
 public class SystemDBOperationList extends OperationList {
-    public SystemDBOperationList() {
+    private void addSimpleSystemDBOperation(String oldVersion, String newVersion, String sqlScriptFileName) {
         getOperationList().add(new SimpleSystemDBOperation(
-                new DefaultArtifactVersion("0.18.3"), new DefaultArtifactVersion("0.18.4"),
-                "io/apitestbase/upgrade/db/SystemDB_0_18_3_To_0_18_4.sql"));
+                new DefaultArtifactVersion(oldVersion), new DefaultArtifactVersion(newVersion),
+                "io/apitestbase/upgrade/db/" + sqlScriptFileName));
+    }
+    public SystemDBOperationList() {
+        addSimpleSystemDBOperation("0.18.3", "0.18.4","SystemDB_0_18_3_To_0_18_4.sql");
 
         //  two operations for 0.18.4 to 0.18.5
         getOperationList().add(new AdvancedSystemDBOperation_0_18_4_To_0_18_5_Part1());
-        getOperationList().add(
-                new SimpleSystemDBOperation(new DefaultArtifactVersion("0.18.4"), new DefaultArtifactVersion("0.18.5"),
-                "io/apitestbase/upgrade/db/SystemDB_0_18_4_To_0_18_5_Part2.sql"));
+        addSimpleSystemDBOperation("0.18.4", "0.18.5","SystemDB_0_18_4_To_0_18_5_Part2.sql");
 
-        getOperationList().add(new SimpleSystemDBOperation(
-                new DefaultArtifactVersion("0.18.5"), new DefaultArtifactVersion("0.18.6"),
-                "io/apitestbase/upgrade/db/SystemDB_0_18_5_To_0_18_6.sql"));
-
-        getOperationList().add(new SimpleSystemDBOperation(
-                new DefaultArtifactVersion("0.18.7"), new DefaultArtifactVersion("0.19.0"),
-                "io/apitestbase/upgrade/db/SystemDB_0_18_7_To_0_19_0.sql"));
-
-        getOperationList().add(new SimpleSystemDBOperation(
-                new DefaultArtifactVersion("0.19.0"), new DefaultArtifactVersion("0.20.0"),
-                "io/apitestbase/upgrade/db/SystemDB_0_19_0_To_0_20_0.sql"));
+        addSimpleSystemDBOperation("0.18.5", "0.18.6","SystemDB_0_18_5_To_0_18_6.sql");
+        addSimpleSystemDBOperation("0.18.7", "0.19.0","SystemDB_0_18_7_To_0_19_0.sql");
+        addSimpleSystemDBOperation("0.19.0", "0.20.0","SystemDB_0_19_0_To_0_20_0.sql");
     }
 }
