b5af8087d9b014cfacb6f3f8e335e9216de47a3d
Zheng Wang <zheng-wang@users.noreply.github.com>
2022-01-28 18:39:59 +0800

    repeated test step run

--- apitestbase-core-server/src/main/java/io/apitestbase/models/teststep/RepeatFixedNumberOfTimesTeststepRunPattern.java
+++ apitestbase-core-server/src/main/java/io/apitestbase/models/teststep/RepeatFixedNumberOfTimesTeststepRunPattern.java
@@ -1,4 +1,13 @@
 package io.apitestbase.models.teststep;
 
 public class RepeatFixedNumberOfTimesTeststepRunPattern extends TeststepRunPattern {
+    private String repeatTimes = "1";
+
+    public String getRepeatTimes() {
+        return repeatTimes;
+    }
+
+    public void setRepeatTimes(String repeatTimes) {
+        this.repeatTimes = repeatTimes;
+    }
 }
--- apitestbase-core-server/src/main/java/io/apitestbase/models/teststep/RepeatUntilPassTeststepRunPattern.java
+++ apitestbase-core-server/src/main/java/io/apitestbase/models/teststep/RepeatUntilPassTeststepRunPattern.java
@@ -1,13 +1,22 @@
 package io.apitestbase.models.teststep;
 
 public class RepeatUntilPassTeststepRunPattern extends TeststepRunPattern {
-    private String intervalBetweenRepeatRuns = "1000";
+    private String waitBetweenRepeatRuns = "1000";
+    private String timeout = "15000";
 
-    public String getIntervalBetweenRepeatRuns() {
-        return intervalBetweenRepeatRuns;
+    public String getWaitBetweenRepeatRuns() {
+        return waitBetweenRepeatRuns;
     }
 
-    public void setIntervalBetweenRepeatRuns(String intervalBetweenRepeatRuns) {
-        this.intervalBetweenRepeatRuns = intervalBetweenRepeatRuns;
+    public void setWaitBetweenRepeatRuns(String waitBetweenRepeatRuns) {
+        this.waitBetweenRepeatRuns = waitBetweenRepeatRuns;
+    }
+
+    public String getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(String timeout) {
+        this.timeout = timeout;
     }
 }
